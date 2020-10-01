8e9865029e22a2a75c090a8811845cab93006953
madeleine <yuema0415@gmail.com>
2020-10-01 18:15:37 -0400

    added config

--- flaskgame/__init__.py
+++ flaskgame/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'bd95b20cb68c470de174216f1ead5b9c'
+app.config['SECRET_KEY'] = config.secret_keys['secret_key']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
